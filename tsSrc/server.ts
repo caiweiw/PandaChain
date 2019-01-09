a7aca5f7a9d8d3a8a24ac41dec7cef47fdce1ed4
caiweiw <caiweiw@hotmail.com>
2019-01-09 16:28:55 +0800

    1111

---
+++ tsSrc/server.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+const app = express();
+app.get('/', function (req, res) {
+  res.end('hello, world');
+})
+
+app.listen(3000, function () {  console.log('server is listening');
+});
